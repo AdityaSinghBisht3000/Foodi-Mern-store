5abd2e1ddce10118d2525f16afe33fcc12ff4c41
AdityaSinghBisht <adityasinghbisht1000001@gmail.com>
2024-06-23 15:53:58 +0530

    created banner component with changes in public folder

--- foodie-client/src/components/Navbar.jsx
+++ foodie-client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import logo from "../../public/logo.png";
+import { FaRegUser } from "react-icons/fa";
 const Navbar = () => {
   const [isSticky, setSticky] = useState(false);
 
@@ -142,7 +143,7 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge badge-sm indicator-item">8</span>
+              <span className="badge badge-sm indicator-item">*</span>
             </div>
           </label>
 
@@ -155,7 +156,7 @@ const Navbar = () => {
               onClick={() => document.getElementById("my_modal_5").showModal()}
               className="flex items-center gap-2 px-6 text-white rounded-full btn bg-green"
             >
-              {/* <FaRegUser /> */}
+              <FaRegUser />
               Login
             </button>
           }
